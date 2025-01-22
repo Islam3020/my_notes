81c2da26d1d9b916424e24690b681857eb7e8894
eslam3020 <ia1961@fayoum.edu.eg>
2025-01-22 11:27:09 +0200

    fimish notes bottom sheet

--- lib/views/my_widgets/add_note_bottom_sheet.dart
+++ lib/views/my_widgets/add_note_bottom_sheet.dart
@@ -1,5 +1,7 @@
 
 import 'package:flutter/material.dart';
+import 'package:my_notes/costant.dart';
+import 'package:my_notes/views/my_widgets/custom_button.dart';
 import 'package:my_notes/views/my_widgets/custom_textfield.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -9,12 +11,17 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal:16),
-      child: Column(children: [
-        SizedBox(height: 32,),
-        CustomTextfield(hint: "title",),
-        SizedBox(height: 16,),
-        CustomTextfield(hint: "content",maxLines: 5,)
-        ],),
+      child: SingleChildScrollView(
+        child: Column(children: [
+          SizedBox(height: 32,),
+          CustomTextfield(hint: "title",),
+          SizedBox(height: 16,),
+          CustomTextfield(hint: "content",maxLines: 5,),
+           SizedBox(height: 30,),
+          CustomButton(),
+           SizedBox(height: 16,)
+          ],),
+      ),
     );
   }
 }
