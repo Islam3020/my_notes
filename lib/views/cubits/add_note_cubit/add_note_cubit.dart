c912560c28a318e55c5b08262e82e0265fb433d3
islam3020 <islamali3020@gmail.com>
2025-01-28 11:30:11 +0200

    add add note cubit to add note bottom sheet

--- lib/views/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/views/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,6 +1,8 @@
-import 'package:bloc/bloc.dart';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
-import 'package:meta/meta.dart';
+
 import 'package:my_notes/costant.dart';
 import 'package:my_notes/models/note_model.dart';
 
@@ -8,6 +10,7 @@ part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
+  
 
   addNote(NoteModel note)async
   {
